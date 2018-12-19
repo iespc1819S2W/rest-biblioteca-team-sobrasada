afb0993a9407fadb933f1cc7bbfb0df46b7057dc
Luz <Luz@DESKTOP-DQ5USUU>
2018-12-19 20:55:57 +0100

    Alta llibres complet amb htaccess

--- autoralta.php
+++ autoralta.php
@@ -1,14 +1,15 @@
 <?php
 //
  $base = __DIR__;
- require_once("$base/model/llibre.class.php");
- $llibre=new Llibre();
- if (isset($_POST["titol"])) {
-     $titol=$_POST["titol"];
-     $res=$titol->insert(array("titol"=>$titol));
+ require_once("$base/model/autor.class.php");
+ $autor=new Autor();
+ if (isset($_POST["nom_aut"])) {
+     $nom_aut=$_POST["nom_aut"];
+     $nacionalitat=isset($_POST["nacionalitat"])?$_POST["nacionalitat"]:'';
+     $res=$autor->insert(array("nom_aut"=>$nom_aut,"fk_nacionalitat"=>$nacionalitat));
  } else {
      $res=new Resposta();
-     $res->SetCorrecta(false,"titol requerit");
+     $res->SetCorrecta(false,"nom_aut requerit");
  }
  header('Content-type: application/json');
  echo json_encode($res); 
